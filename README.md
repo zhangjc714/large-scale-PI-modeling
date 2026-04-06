5da126ac36158cc415af8a24768c530fdd0acdaf
ZhangJC <798639808@qq.com>
2026-04-06 15:59:40 +0800

    update paper info

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Knowledge-Driven Large-Scale Multi-Objective Evolutionary Learning for Interval Prediction of Key Quality Indicators in Blast Furnace Ironmaking Process
+# Interval Prediction for Blast Furnace Ironmaking
+
+Paper: <https://doi.org/10.1016/j.swevo.2026.102379> (SWEVO 2026)
+
+If you use this project or its code in a publication, please cite the associated work:
+
+```latex
+@article{zhang2026KnowledgedrivenLargescale,
+  title = {Knowledge-Driven Large-Scale Multi-Objective Evolutionary Learning for Interval Prediction of Key Quality Indicators in Blast Furnace Ironmaking Process},
+  author = {Zhang, Jingchuan and Liu, Yaxue and Wang, Xianpeng},
+  year = 2026,
+  month = apr,
+  journal = {Swarm and Evolutionary Computation},
+  volume = {104},
+  pages = {102379},
+  issn = {22106502},
+  doi = {10.1016/j.swevo.2026.102379}
+}
+```
--- src/algorithm/DKL_LSMOEA.m
+++ src/algorithm/DKL_LSMOEA.m
@@ -1,6 +1,22 @@
 classdef DKL_LSMOEA < ALGORITHM
 % DKL-LSMOEA: Dual Knowledge Learning-based Large-Scale Multi-Objective Evolutionary Algorithm
 
+%------------------------------- Reference --------------------------------
+% J. Zhang, Y. Liu, and X. Wang. Knowledge-driven large-scale multi-objective 
+% evolutionary learning for interval prediction of key quality indicators in 
+% blast furnace ironmaking process. Swarm and Evolutionary Computation, 
+% vol. 104, p. 102379, Apr. 2026.
+%------------------------------- Copyright --------------------------------
+% Copyright (c) 2025 BIMK Group. You are free to use the PlatEMO for
+% research purposes. All publications which use this platform or any code
+% in the platform should acknowledge the use of "PlatEMO" and reference "Ye
+% Tian, Ran Cheng, Xingyi Zhang, and Yaochu Jin, PlatEMO: A MATLAB platform
+% for evolutionary multi-objective optimization [educational forum], IEEE
+% Computational Intelligence Magazine, 2017, 12(4): 73-87".
+%--------------------------------------------------------------------------
+
+% This function is written by Jingchuan Zhang
+
     methods
         function main(Algorithm,Problem)
             %% Parameter setting
